0b5de570dc794332751eb1f48ce1a9f9bcfe9169
Soumyo <h.soumyo@gmail.com>
2024-02-25 00:24:18 +0530

    A simple Working Calculator

---
+++ script.js
@@ -0,0 +1,79 @@
+let str="";
+function one(){
+    str=str+'1';
+    document.querySelector(".text").innerHTML=str;
+}
+function two(){
+    str=str+'2';
+    document.querySelector(".text").innerHTML=str;
+}
+function three(){
+    str=str+'3';
+    document.querySelector(".text").innerHTML=str;
+}
+function four(){
+    str=str+'4';
+    document.querySelector(".text").innerHTML=str;
+}
+function five(){
+    str=str+'5';
+    document.querySelector(".text").innerHTML=str;
+}
+function six(){
+    str=str+'6';
+    document.querySelector(".text").innerHTML=str;
+}
+function seven(){
+    str=str+'7';
+    document.querySelector(".text").innerHTML=str;
+}
+function eight(){
+    str=str+'8';
+    document.querySelector(".text").innerHTML=str;
+}
+function nine(){
+    str=str+'9';
+    document.querySelector(".text").innerHTML=str;
+}
+function zero(){
+    str=str+'0';
+    document.querySelector(".text").innerHTML=str;
+}
+function dot(){
+    str=str+'.';
+    document.querySelector(".text").innerHTML=str;
+}
+
+function plus(){
+    str=str+'+';
+    document.querySelector(".text").innerHTML=str;
+}
+function minus(){
+    str=str+'-';
+    document.querySelector(".text").innerHTML=str;
+}
+function mult(){
+    str=str+'*';
+    document.querySelector(".text").innerHTML=str;
+}
+function divi(){
+    str=str+'/';
+    document.querySelector(".text").innerHTML=str;
+}
+function mod(){
+    str=str+'%';
+    document.querySelector(".text").innerHTML=str;
+}
+function equal(){
+    str=eval(str);
+    document.querySelector(".text").innerHTML=str;
+    str="";
+}
+function clean(){
+    str="";
+    document.querySelector(".text").innerHTML=str;
+}
+function erase(){
+    str=str.slice(0,str.length-1);
+    document.querySelector(".text").innerHTML=str;
+}
